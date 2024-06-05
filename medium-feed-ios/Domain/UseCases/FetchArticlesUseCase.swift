75e5e4b8565e0a28539bf2e1030749a8898255c4
Tanawat Polsuwan <numpun@gmail.com>
2024-06-05 15:58:25 +0700

    remove: comment

--- medium-feed-ios/Domain/UseCases/FetchArticlesUseCase.swift
+++ medium-feed-ios/Domain/UseCases/FetchArticlesUseCase.swift
@@ -6,31 +6,6 @@
 //
 
 import Foundation
-
-//class FetchArticlesUseCase {
-//    private let repository: ArticleRepository
-//
-//    init(repository: ArticleRepository) {
-//        self.repository = repository
-//    }
-//
-//    func fetchArticles(completion: @escaping ([Article]) -> Void) {
-//        repository.loadLocalArticles { articles in
-//            completion(articles)
-//        }
-//        
-//        repository.fetchArticles { result in
-//            switch result {
-//            case .success(let articles):
-//                self.repository.saveArticles(articles)
-//                completion(articles)
-//            case .failure(let error):
-//                print("Error fetching articles:", error)
-//            }
-//        }
-//    }
-//}
-import Foundation
 import Combine
 
 class FetchArticlesUseCase {
